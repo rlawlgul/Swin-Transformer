52fa76a35691cd4808a94845ae1204291d6395ae
rlawlgul <100881552+rlawlgul@users.noreply.github.com>
2022-03-11 22:33:01 -0500

    test2
    deleted

--- models/swin_transformer.py
+++ models/swin_transformer.py
@@ -10,7 +10,7 @@ import torch.nn as nn
 import torch.utils.checkpoint as checkpoint
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
-#jh
+
 class Mlp(nn.Module):
     def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0.):
         super().__init__()
